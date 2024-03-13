28999ba0f8dc5cb54ba7fbc576afa6de911b0def
P4UL-M <paul.mairesse@free.fr>
2024-03-13 13:43:45 +0100

    update wave countdecount

--- MyComptDecomptNBits/mytbcompdecompnbits_SA.vhd
+++ MyComptDecomptNBits/mytbcompdecompnbits_SA.vhd
@@ -32,6 +32,11 @@ architecture mycomptdecompNbitstestbench_Arch of mycomptdecompNbitstestbench is
     signal s1_sim : std_logic_vector(N-1 downto 0) := (others => '0'); 
     signal reset_sim, cpt_sim, clock_sim	: std_logic := '0';
     signal cpt_calc : integer := 0;
+    
+    -- Déclaration des états de l'automate
+    type t_SM_Test is (S0_INITCPT, S1_CPT, S2_INITDCPT, S3_DCPT, S4_END);
+    signal r_SM_Test : t_SM_Test := S0_INITCPT;
+    signal reset_automate : std_logic := '0';
 
 begin
 
